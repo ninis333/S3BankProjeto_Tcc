849c378523b93322f9caa9fbaf17d1e3e7167c8d
ninis333 <ninise755@gmail.com>
2026-09-16 10:52:25 -0300

    header da pagina home pronta

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,41 @@ import './Home.css'
 import '././../../index.css'
 import '././../../styles/tokens.css'
 
+export default Home
+
 function Home() {
-	return <main className="home">Home</main>
+	return (
+		<main className="home">
+			<div className="containerheader">
+				<Header />
+				<BodyHome/>
+			</div>
+		</main>
+	)
 }
 
-export default Home
+export const Header = () => {
+	return (
+		<div className="header">
+			<div className="linksEsquerda">
+			<p className="links">Conta</p>
+			<p className="links">Cartões</p>
+			<p className="links">App</p>
+			<p className="links">Segurança</p>
+			</div>
+			<div className="linksDireita">
+			<p className="linkEntrar">Entrar</p>
+			<button className='botaoConta'>Abrir minha conta</button>
+			</div>
+		</div>
+
+	)
+}
+
+export const BodyHome = () => {
+  return (
+    <div className="bodyHome">
+		<p className='pInicial'>· CONTA DIGITAL · CARTÂO DE METAL</p>
+    </div>
+  );
+}
